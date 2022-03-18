695886043fb14daa6590ae4d1ac42cc013d658fa
Akshay Chaudhari <er.akshayrc@gmail.com>
2022-03-18 23:33:03 +0530

    review page added.

--- basicFeedbackApplication/handler.js
+++ basicFeedbackApplication/handler.js
@@ -1,13 +1,31 @@
+var fs = require('fs');
+
 function home(request, response) {
     console.log("Executing home handler");
-    response.writeHead(200);
-    response.end("Home handler...");
+    fs.readFile('reviewFormPage.html', function (err, data) {
+        if (err) {
+            console.log("Error reading the review file");
+            response.writeHead(500);
+            response.end("Review handler...");
+        } else {
+            response.writeHead(200, { 'content-type': 'text/html' });
+            response.write(data);
+            response.end()
+        }
+    });
 }
 
 function review(request, response) {
     console.log("Executing review handler");
-    response.writeHead(200);
-    response.end("Review handler...");
+    var reviewData = "";
+    request.on('data', function (chunk) {
+        reviewData += chunk;
+    })
+    request.on('end', function () {
+        response.writeHead(200, { 'content-type': 'text/text' });
+        response.write(reviewData);
+        response.end()
+    })
 }
 
 module.exports.home = home;
