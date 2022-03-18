8dce5a2390826074942479f7cec184467bb42ae2
Akshay Chaudhari <er.akshayrc@gmail.com>
2022-03-19 01:39:41 +0530

    webapp se start!

--- basicFeedbackApplication/handler.js
+++ basicFeedbackApplication/handler.js
@@ -2,13 +2,13 @@ var fs = require('fs');
 
 function home(request, response) {
     console.log("Executing home handler");
-    fs.readFile('reviewFormPage.html', function (err, data) {
+    fs.readFile('review.html', function (err, data) {
         if (err) {
             console.log("Error reading the review file");
             response.writeHead(500);
             response.end("Review handler...");
         } else {
-            response.writeHead(200, { 'content-type': 'text/html' });
+            response.writeHead(200, { 'Content-Type': 'text/html' });
             response.write(data);
             response.end()
         }
@@ -18,14 +18,16 @@ function home(request, response) {
 function review(request, response) {
     console.log("Executing review handler");
     var reviewData = "";
+
     request.on('data', function (chunk) {
-        reviewData += chunk;
-    })
+        reviewData = reviewData + chunk;
+    });
     request.on('end', function () {
-        response.writeHead(200, { 'content-type': 'text/text' });
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
         response.write(reviewData);
-        response.end()
+        response.end();
     })
+
 }
 
 module.exports.home = home;
