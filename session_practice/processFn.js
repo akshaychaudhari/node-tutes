a08450a0f81e4d81108f1987c004d830dd58c726
Akshay Chaudhari <er.akshayrc@gmail.com>
2022-03-25 23:32:18 +0530

    Working with stdin and stdout

--- session_practice/processFn.js
+++ session_practice/processFn.js
@@ -1,6 +1,18 @@
 console.log(process.pid)
 console.log(process.versions.node)
 console.log(process.argv)
-
+console.error("=================================")
 const [, , firstName, lastName] = process.argv;
 console.log(`Your name is ${firstName} ${lastName}`)
+console.error("=================================")
+const grab = flag => {
+    let indexAfterFlag = process.argv.indexOf(flag) + 1;
+    return process.argv[indexAfterFlag];
+}
+const greeting = grab("--greeting")
+const user = grab("--user")
+console.log("For u:" + ` ${greeting} ${user}`)
+console.log("=================================")
+
+process.stdout.write("Hello ")
+process.stdout.write("World \t Noob \n\n\n")
